733a6645b80512912e7604c27ebbad62109d50d1
Rav Kant <ravikant.zyd@gmail.com>
2023-02-17 20:02:56 +0630

    Add project files.

---
+++ BlazorCrud/Services/EmployeeService.cs
@@ -0,0 +1,61 @@
+﻿using BlazorCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly DatabaseContext _ctx;
+        public EmployeeService(DatabaseContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public bool AddUpdate(Employee employee)
+        {
+            try
+            {
+                if (employee.Id == 0)
+                    _ctx.Employee.Add(employee);
+                else
+                    _ctx.Employee.Update(employee);
+                _ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var employee = FindById(id);
+                if (employee == null)
+                    return false;
+                _ctx.Employee.Remove(employee);
+                _ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+        }
+
+        public Employee FindById(int id)
+        {
+            return _ctx.Employee.Find(id);
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _ctx.Employee.ToList();
+        }
+    }
+}
---
+++ BlazorCrud/Services/IEmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using BlazorCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Services
+{
+    public interface IEmployeeService
+    {
+        bool AddUpdate(Employee employee);
+        bool Delete(int id);
+        Employee FindById(int id);
+        List<Employee> GetAll();
+    }
+}
